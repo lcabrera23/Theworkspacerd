d888b3df565d428aed1e5739c987b192e8d6df26
Luis Miguel Cabrera Garcia <ce_1940@hotmail.es>
2021-02-06 15:44:43 -0400

    subiendo los precios

--- Theworkspacerd.Web/Controllers/HomeController.cs
+++ Theworkspacerd.Web/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Theworkspacerd.Web.Models;
 
@@ -32,7 +34,89 @@ namespace Theworkspacerd.Web.Controllers
             return View();
         }
 
-        public IActionResult Nosotros()
+        public IActionResult Planes()
+        {
+
+            return View();
+        }
+        public IActionResult Pagos()
+        {
+			
+			return View();
+        }
+
+		public IActionResult Pagos2() {
+    //        var client = new WebClient();
+    //        client.Headers.Add("content-type", "application/json");
+    //        client.Headers.Add("authorization", "Bearer A21AAFJ9eoorbnbVH3fTJrCTl2o7-P_1T6q8vdYB_QwBB9Ais5ZZmJD4BsNjIiOh8j8OyOcfzLO1BKcgKe0pK-mntpk6jOm-g");
+    //        client.Headers.Add("accept", "application/json");
+
+    //        var body = @"{
+				//  ""sender_batch_header"": {
+				//    ""email_subject"": ""You have a payment"",
+				//    ""sender_batch_id"": ""batch-1612486420098""
+				//  },
+				//  ""items"": [
+				//    {
+				//      ""recipient_type"": ""PHONE"",
+				//      ""amount"": {
+				//        ""value"": ""1.00"",
+				//        ""currency"": ""USD""
+				//      },
+				//      ""receiver"": ""4087811638"",
+				//      ""note"": ""Payouts sample transaction"",
+				//      ""sender_item_id"": ""item-1-1612486420098""
+				//    },
+				//    {
+				//      ""recipient_type"": ""EMAIL"",
+				//      ""amount"": {
+				//        ""value"": ""1.00"",
+				//        ""currency"": ""USD""
+				//      },
+				//      ""receiver"": ""ps-rec@paypal.com"",
+				//      ""note"": ""Payouts sample transaction"",
+				//      ""sender_item_id"": ""item-2-1612486420098""
+				//    },
+				//    {
+				//      ""recipient_type"": ""PAYPAL_ID"",
+				//      ""amount"": {
+				//        ""value"": ""1.00"",
+				//        ""currency"": ""USD""
+				//      },
+				//      ""receiver"": ""FSMRBANCV8PSG"",
+				//      ""note"": ""Payouts sample transaction"",
+				//      ""sender_item_id"": ""item-3-1612486420098""
+				//    }
+				//  ]
+				//}";
+
+    //        try
+    //        {
+    //            var response = client.UploadString("https://api.sandbox.paypal.com/v1/payments/payouts", "POST", body);
+    //            Console.WriteLine(response);
+
+    //            // Keep the console window open in debug mode.
+    //            Console.ReadKey();
+    //        }
+    //        catch (WebException e)
+    //        {
+    //            var errorResponse = e.Response as HttpWebResponse;
+    //            Console.WriteLine(e.Response.Headers);
+    //            string responseText;
+    //            using (var reader = new StreamReader(errorResponse.GetResponseStream()))
+    //            {
+    //                responseText = reader.ReadToEnd();
+    //                Console.WriteLine(responseText);
+    //            }
+    //            // Keep the console window open in debug mode.
+    //            Console.ReadKey();
+    //        }
+
+            return View();
+		}
+
+
+		public IActionResult Nosotros()
         {
             return View();
         }
